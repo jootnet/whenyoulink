8e353cdd9aed2c5de3b5c2a839191920f26c131e
jootnet <shawryan@outlook.com>
2022-12-01 20:33:31 +0800

    支持鼠标、键盘操作

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 
 #include <sstream>
+#include <regex>
+#include <tuple>
 
 #include "sciter-x.h"
 #include "sciter-x-window.hpp"
 
 #include "wrd.h"
+
+static std::vector<std::string> c_split(const char* in, const char* delim);
+static std::vector<std::tuple<std::string, std::string, int>> keys;
 
 static sciter::om::hasset<sciter::window> pwin;
 
@@ -98,6 +103,165 @@ private:
 		pwin->broadcast_event(evt);
 	}
 	static void OnRemoteStringReceive(const WRDClient client, const char* data, void* usr_param) {
+		std::cout << data << std::endl;
+		if (std::strncmp(data, "mousedown ", 10) == 0) {
+			INPUT input = { };
+			input.type = INPUT_MOUSE;
+			auto tokens = c_split(data, " ");
+			if (tokens.size() < 4) return;
+			int x, y, button;
+			try {
+				x = std::stoi(tokens[1]);
+				y = std::stoi(tokens[2]);
+				button = std::stoi(tokens[3]);
+			}
+			catch (const std::invalid_argument&) {
+				return;
+			}
+			catch (const std::out_of_range&) {
+				return;
+			}
+			if (button == 0) {
+				input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+			}
+			else if (button == 1) {
+				input.mi.dwFlags = MOUSEEVENTF_MIDDLEDOWN;
+			}
+			else if (button == 2) {
+				input.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+			}
+			else if (button == 3) {
+				input.mi.dwFlags = MOUSEEVENTF_XDOWN;
+				input.mi.mouseData = XBUTTON1;
+			}
+			else if (button == 4) {
+				input.mi.dwFlags = MOUSEEVENTF_XDOWN;
+				input.mi.mouseData = XBUTTON2;
+			}
+			input.mi.dwFlags |= MOUSEEVENTF_ABSOLUTE;
+			input.mi.dx = x * (65536/::GetSystemMetrics(SM_CXSCREEN));
+			input.mi.dy = y * (65536 / ::GetSystemMetrics(SM_CYSCREEN));
+			::SendInput(1, &input, sizeof input);
+			return;
+		}
+		if (std::strncmp(data, "mouseup ", 8) == 0) {
+			INPUT input = { };
+			input.type = INPUT_MOUSE;
+			auto tokens = c_split(data, " ");
+			if (tokens.size() < 4) return;
+			int x, y, button;
+			try {
+				x = std::stoi(tokens[1]);
+				y = std::stoi(tokens[2]);
+				button = std::stoi(tokens[3]);
+			}
+			catch (const std::invalid_argument&) {
+				return;
+			}
+			catch (const std::out_of_range&) {
+				return;
+			}
+			if (button == 0) {
+				input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
+			}
+			else if (button == 1) {
+				input.mi.dwFlags = MOUSEEVENTF_MIDDLEUP;
+			}
+			else if (button == 2) {
+				input.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+			}
+			else if (button == 3) {
+				input.mi.dwFlags = MOUSEEVENTF_XUP;
+				input.mi.mouseData = XBUTTON1;
+			}
+			else if (button == 4) {
+				input.mi.dwFlags = MOUSEEVENTF_XUP;
+				input.mi.mouseData = XBUTTON2;
+			}
+			input.mi.dwFlags |= MOUSEEVENTF_ABSOLUTE;
+			input.mi.dx = x * (65536 / ::GetSystemMetrics(SM_CXSCREEN));
+			input.mi.dy = y * (65536 / ::GetSystemMetrics(SM_CYSCREEN));
+			::SendInput(1, &input, sizeof input);
+			return;
+		}
+		if (std::strncmp(data, "mousemove ", 10) == 0) {
+			INPUT input = { };
+			input.type = INPUT_MOUSE;
+			auto tokens = c_split(data, " ");
+			if (tokens.size() < 3) return;
+			int x, y;
+			try {
+				x = std::stoi(tokens[1]);
+				y = std::stoi(tokens[2]);
+			}
+			catch (const std::invalid_argument&) {
+				return;
+			}
+			catch (const std::out_of_range&) {
+				return;
+			}
+			input.mi.dwFlags = MOUSEEVENTF_MOVE;
+			input.mi.dwFlags |= MOUSEEVENTF_ABSOLUTE;
+			input.mi.dx = x * (65536 / ::GetSystemMetrics(SM_CXSCREEN));
+			input.mi.dy = y * (65536 / ::GetSystemMetrics(SM_CYSCREEN));
+			::SendInput(1, &input, sizeof input);
+			return;
+		}
+		if (std::strncmp(data, "wheel ", 6) == 0) {
+			INPUT input = { };
+			input.type = INPUT_MOUSE;
+			auto tokens = c_split(data, " ");
+			if (tokens.size() < 5) return;
+			double x, y, z;
+			int mode;
+			try {
+				x = std::stod(tokens[1]);
+				y = std::stod(tokens[2]);
+				z = std::stod(tokens[3]);
+				mode = std::stoi(tokens[4]);
+			}
+			catch (const std::invalid_argument&) {
+				return;
+			}
+			catch (const std::out_of_range&) {
+				return;
+			}
+			if (mode != 0) return;
+			if (x != 0) {
+				input.mi.mouseData = x > 0 ? WHEEL_DELTA : -WHEEL_DELTA;
+				input.mi.dwFlags = MOUSEEVENTF_HWHEEL;
+			}
+			else if (y != 0) {
+				input.mi.mouseData = y > 0 ? WHEEL_DELTA : -WHEEL_DELTA;
+				input.mi.dwFlags = MOUSEEVENTF_WHEEL;
+			}
+			else return;
+			::SendInput(1, &input, sizeof input);
+			return;
+		}
+		if (std::strncmp(data, "keydown ", 8) == 0 || std::strncmp(data, "keyup ", 6) == 0) {
+			auto tokens = c_split(data, " ");
+			if (tokens.size() < 3) return;
+			auto up = std::strncmp(data, "keyup ", 6) == 0;
+			INPUT input = {};
+			input.type = INPUT_KEYBOARD;
+			if (up) {
+				input.ki.dwFlags = KEYEVENTF_KEYUP;
+			}
+			auto find = false;
+			for (auto it = keys.begin(); it != keys.end(); ++it) {
+				if (std::get<0>(*it) == tokens[1]/* && std::get<1>(*it) == tokens[2]*/) { // 暂不校验key，因为存在大小写和不同类型键盘，还没考虑好怎么做兼容
+					input.ki.wVk = std::get<2>(*it);
+					find = true;
+					break;
+				}
+			}
+			if (find) {
+				::SendInput(1, &input, sizeof input);
+			}
+			return;
+		}
+
 		auto webrtc = reinterpret_cast<Webrtc*>(usr_param);
 		std::stringstream ss;
 		ss << "{\"type\":\"remote_string\", \"peer_id\":\"" << aux::w2a(webrtc->peer_id_);
@@ -126,6 +290,117 @@ public:
 #include "resources.cpp"
 
 int uimain(std::function<int()> run) {
+	//https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes
+	keys.push_back(std::tuple<std::string, std::string, int>("Escape", "Escape", VK_ESCAPE));
+	keys.push_back(std::tuple<std::string, std::string, int>("F1", "F1", VK_F1));
+	keys.push_back(std::tuple<std::string, std::string, int>("F2", "F2", VK_F2));
+	keys.push_back(std::tuple<std::string, std::string, int>("F3", "F3", VK_F3));
+	keys.push_back(std::tuple<std::string, std::string, int>("F4", "F4", VK_F4));
+	keys.push_back(std::tuple<std::string, std::string, int>("F5", "F5", VK_F5));
+	keys.push_back(std::tuple<std::string, std::string, int>("F6", "F6", VK_F6));
+	keys.push_back(std::tuple<std::string, std::string, int>("F7", "F7", VK_F7));
+	keys.push_back(std::tuple<std::string, std::string, int>("F8", "F8", VK_F8));
+	keys.push_back(std::tuple<std::string, std::string, int>("F9", "F9", VK_F9));
+	keys.push_back(std::tuple<std::string, std::string, int>("F10", "F10", VK_F10));
+	keys.push_back(std::tuple<std::string, std::string, int>("F11", "F11", VK_F11));
+	keys.push_back(std::tuple<std::string, std::string, int>("F12", "F12", VK_F12));
+	keys.push_back(std::tuple<std::string, std::string, int>("ScrollLock", "ScrollLock", VK_SCROLL));
+	keys.push_back(std::tuple<std::string, std::string, int>("Pause", "Pause", VK_PAUSE));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit1", "1", '1'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit2", "2", '2'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit3", "3", '3'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit4", "4", '4'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit5", "5", '5'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit6", "6", '6'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit7", "7", '7'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit8", "8", '8'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit9", "9", '9'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Digit0", "0", '0'));
+	keys.push_back(std::tuple<std::string, std::string, int>("Backspace", "Backspace", VK_BACK));
+	keys.push_back(std::tuple<std::string, std::string, int>("Insert", "Insert", VK_INSERT));
+	keys.push_back(std::tuple<std::string, std::string, int>("Home", "Home", VK_HOME));
+	keys.push_back(std::tuple<std::string, std::string, int>("PageUp", "PageUp", VK_PRIOR));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumLock", "NumLock", VK_NUMLOCK));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadDivide", "/", VK_DIVIDE));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadMultiply", "*", VK_MULTIPLY));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadSubtract", "-", VK_SUBTRACT));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadAdd", "+", VK_ADD));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad9", "9", VK_NUMPAD9));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad8", "8", VK_NUMPAD8));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad7", "7", VK_NUMPAD7));
+	keys.push_back(std::tuple<std::string, std::string, int>("PageDown", "PageDown", VK_NEXT));
+	keys.push_back(std::tuple<std::string, std::string, int>("End", "End", VK_END));
+	keys.push_back(std::tuple<std::string, std::string, int>("Delete", "Delete", VK_DELETE));
+	keys.push_back(std::tuple<std::string, std::string, int>("Tab", "Tab", VK_TAB));
+	keys.push_back(std::tuple<std::string, std::string, int>("CapsLock", "CapsLock", VK_CAPITAL));
+	keys.push_back(std::tuple<std::string, std::string, int>("Enter", "Enter", VK_RETURN));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad4", "4", VK_NUMPAD4));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad5", "5", VK_NUMPAD5));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad6", "6", VK_NUMPAD6));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad3", "3", VK_NUMPAD3));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad2", "2", VK_NUMPAD2));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad1", "1", VK_NUMPAD1));
+	keys.push_back(std::tuple<std::string, std::string, int>("ArrowUp", "ArrowUp", VK_UP));
+	keys.push_back(std::tuple<std::string, std::string, int>("ShiftRight", "Shift", VK_RSHIFT));
+	keys.push_back(std::tuple<std::string, std::string, int>("ShiftLeft", "Shift", VK_LSHIFT));
+	keys.push_back(std::tuple<std::string, std::string, int>("ControlLeft", "Control", VK_LCONTROL));
+	keys.push_back(std::tuple<std::string, std::string, int>("MetaLeft", "Meta", VK_LWIN));
+	keys.push_back(std::tuple<std::string, std::string, int>("AltLeft", "Alt", VK_LMENU));
+	keys.push_back(std::tuple<std::string, std::string, int>("Space", " ", VK_SPACE));
+	keys.push_back(std::tuple<std::string, std::string, int>("AltRight", "Alt", VK_RMENU));
+	keys.push_back(std::tuple<std::string, std::string, int>("MetaRight", "Meta", VK_RWIN));
+	keys.push_back(std::tuple<std::string, std::string, int>("ControlRight", "Control", VK_RCONTROL));
+	keys.push_back(std::tuple<std::string, std::string, int>("ArrowLeft", "ArrowLeft", VK_LEFT));
+	keys.push_back(std::tuple<std::string, std::string, int>("ArrowDown", "ArrowDown", VK_DOWN));
+	keys.push_back(std::tuple<std::string, std::string, int>("ArrowRight", "ArrowRight", VK_RIGHT));
+	keys.push_back(std::tuple<std::string, std::string, int>("Numpad0", "0", VK_NUMPAD0));
+	keys.push_back(std::tuple<std::string, std::string, int>("AudioVolumeDown", "AudioVolumeDown", VK_VOLUME_DOWN));
+	keys.push_back(std::tuple<std::string, std::string, int>("AudioVolumeUp", "AudioVolumeUp", VK_VOLUME_UP));
+	keys.push_back(std::tuple<std::string, std::string, int>("AudioVolumeMute", "AudioVolumeMute", VK_VOLUME_MUTE));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyA", "A", (int)'A'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyB", "B", (int)'B'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyC", "C", (int)'C'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyD", "D", (int)'D'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyE", "E", (int)'E'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyF", "F", (int)'F'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyG", "G", (int)'G'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyH", "H", (int)'H'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyI", "I", (int)'I'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyJ", "J", (int)'J'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyK", "K", (int)'K'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyL", "L", (int)'L'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyM", "M", (int)'M'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyN", "N", (int)'N'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyO", "O", (int)'O'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyP", "P", (int)'P'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyQ", "Q", (int)'Q'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyR", "R", (int)'R'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyS", "S", (int)'S'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyT", "T", (int)'T'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyU", "U", (int)'U'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyV", "V", (int)'V'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyW", "W", (int)'W'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyX", "X", (int)'X'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyY", "Y", (int)'Y'));
+	keys.push_back(std::tuple<std::string, std::string, int>("KeyZ", "Z", (int)'Z'));
+
+	// 这些_OEM_有点不太确定
+	keys.push_back(std::tuple<std::string, std::string, int>("Minus", "-", VK_OEM_MINUS));
+	keys.push_back(std::tuple<std::string, std::string, int>("Equal", "=", VK_OEM_PLUS));
+	keys.push_back(std::tuple<std::string, std::string, int>("Period", ".", VK_OEM_PERIOD));
+	keys.push_back(std::tuple<std::string, std::string, int>("Comma", ",", VK_OEM_COMMA));
+	keys.push_back(std::tuple<std::string, std::string, int>("Semicolon", ";", VK_OEM_1));
+	keys.push_back(std::tuple<std::string, std::string, int>("Slash", "/", VK_OEM_2));
+	keys.push_back(std::tuple<std::string, std::string, int>("Backquote", "`", VK_OEM_3));
+	keys.push_back(std::tuple<std::string, std::string, int>("BracketLeft", "[", VK_OEM_4));
+	keys.push_back(std::tuple<std::string, std::string, int>("Backslash", "\\", VK_OEM_5));
+	keys.push_back(std::tuple<std::string, std::string, int>("BracketRight", "]", VK_OEM_6));
+	keys.push_back(std::tuple<std::string, std::string, int>("Quote", "'", VK_OEM_7));
+	// 小键盘的点和回车，没有找到比较像的的vk
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadDecimal", ".", VK_OEM_PERIOD));
+	keys.push_back(std::tuple<std::string, std::string, int>("NumpadEnter", "Enter", VK_RETURN));
+	// windows的键盘右键按钮没有找到对应的值
+	//keys.push_back(std::tuple<std::string, std::string, int>("ContextMenu", "ContextMenu", VK_BACK));
 #ifdef _DEBUG
 	::SciterSetOption(NULL, SCITER_SET_DEBUG_MODE, TRUE);
 	::WinExec("inspector.exe", SW_SHOW);
@@ -146,4 +421,12 @@ int uimain(std::function<int()> run) {
 	pwin->expand();
 
 	return run();
+}
+
+std::vector<std::string> c_split(const char* in, const char* delim) {
+	std::regex re{ delim };
+	return std::vector<std::string> {
+		std::cregex_token_iterator(in, in + std::strlen(in), re, -1),
+			std::cregex_token_iterator()
+	};
 }
